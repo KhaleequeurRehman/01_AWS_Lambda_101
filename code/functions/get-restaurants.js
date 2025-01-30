0778526a556bb016fe5e253b9c6670d5138d7ce8
Khaleeque Ur Rehman <57356787+KhaleequeurRehman@users.noreply.github.com>
2025-01-30 20:49:29 +0500

    remove comments from functions/get-restaurants.js

--- code/functions/get-restaurants.js
+++ code/functions/get-restaurants.js
@@ -1,7 +1,7 @@
 const { DynamoDBClient, ScanCommand } = require("@aws-sdk/client-dynamodb");
 const { marshall, unmarshall } = require("@aws-sdk/util-dynamodb");
 
-const client = new DynamoDBClient({ region: 'us-east-2' }); // Replace 'us-east-1' with your desired AWS region
+const client = new DynamoDBClient({ region: 'us-east-2' });
 const defaultResults = process.env.defaultResults || 8;
 const tableName = process.env.restaurants_table;
 
@@ -43,36 +43,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-
-
-
-
-// const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
-// const dynamodb = new DocumentClient()
-
-// const defaultResults = process.env.defaultResults || 8
-// const tableName = process.env.restaurants_table
-
-// const getRestaurants = async (count) => {
-//   console.log(`fetching ${count} restaurants from ${tableName}...`)
-//   const req = {
-//     TableName: tableName,
-//     Limit: count
-//   }
-//   console.log('tableName ',tableName)
-
-//   const resp = await dynamodb.scan(req).promise()
-//   console.log(`found ${resp.Items.length} restaurants`)
-//   return resp.Items
-// }
-
-// module.exports.handler = async (event, context) => {  
-//   const restaurants = await getRestaurants(defaultResults)
-//   const response = {
-//     statusCode: 200,
-//     body: JSON.stringify(restaurants)
-//   }
-
-//   return response
-// }
